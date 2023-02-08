4ab6043e8e27493d54b6e4d15bba97c10d0139e2
Caleb <55930892+Napcalx@users.noreply.github.com>
2023-02-08 02:04:46 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ const initGame = function () {
     for (let i = wordArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         
-        [wordArray[i]], [wordArray[j]] = [wordArray[j]], [wordArray[i]];
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
     }
     wordText.innerText = wordArray.join('');
     hintText.innerText = randomObj.hint;
